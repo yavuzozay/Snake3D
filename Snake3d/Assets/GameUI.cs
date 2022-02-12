ec8148436c66c533144621c6ca8a52a2c66e1aff
yavuzozay <57958423+yavuzozay@users.noreply.github.com>
2022-02-12 20:51:44 +0300

    add previews

--- Snake3d/Assets/GameUI.cs
+++ Snake3d/Assets/GameUI.cs
@@ -3,28 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class GameUI : MonoBehaviour
 {
 
     private TextMeshProUGUI scoreText,gameOvScore;
     private GameObject GameOverPanel;
     private bool gameOver=false;
+
     Scene scene;
    
 
     private void Awake()
     {
+
         scoreText = transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
         GameOverPanel = transform.GetChild(1).gameObject;
         gameOvScore = GameOverPanel.transform.GetChild(1).transform.GetComponent<TextMeshProUGUI>();
+    
         scene = SceneManager.GetActiveScene();
     }
     private void Start()
     {
         scoreText.text = "Score : 0";
         GameOverPanel.SetActive(gameOver);
+      
     }
-    public void Restrart()
+  
+    public void Restart()
     {
         SceneManager.LoadScene(scene.buildIndex);
         Time.timeScale = 1f;
